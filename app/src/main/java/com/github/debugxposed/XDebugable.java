a43195d5a542e2d4ecaa39aaa732b76d9d9b0d0c
deskid <deskidzhou@gmail.com>
2016-12-05 19:22:58 +0800

    init all

---
+++ app/src/main/java/com/github/debugxposed/XDebugable.java
@@ -0,0 +1,73 @@
+package com.github.debugxposed;
+
+import android.os.Process;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by deskidz on 12/5/16.
+ */
+
+public class XDebugable implements IXposedHookLoadPackage, IXposedHookZygoteInit {
+    private static final int DEBUG_ENABLE_DEBUGGER = 0x1;
+    private XC_MethodHook debugAppsHook = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param)
+                throws Throwable {
+            XposedBridge.log("-- beforeHookedMethod :" + param.args[1]);
+            int id = 5;
+            int flags = (Integer) param.args[id];
+            if ((flags & DEBUG_ENABLE_DEBUGGER) == 0) {
+                flags |= DEBUG_ENABLE_DEBUGGER;
+            }
+            param.args[id] = flags;
+            if (BuildConfig.DEBUG) {
+                XposedBridge.log("-- app debugable flags to 1 :" + param.args[1]);
+            }
+        }
+    };
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+//        if (BuildConfig.DEBUG) {
+//            XposedBridge.log("-- handle package: " + loadPackageParam.packageName + " process: " + loadPackageParam.processName);
+//        }
+//
+//        if (loadPackageParam.appInfo == null ||
+//                (loadPackageParam.appInfo.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) != 0) {
+//            XposedBridge.log("-- appInfo: " + loadPackageParam.appInfo);
+//            return;
+//        }
+//
+//        XposedBridge.log("-- start hook, appInfo: " + loadPackageParam.appInfo);
+//
+//        Method start = findMethodExact(Process.class, "start",
+//                String.class,
+//                String.class,
+//                Integer.TYPE,
+//                Integer.TYPE,
+//                int[].class,
+//                Integer.TYPE,
+//                Integer.TYPE,
+//                Integer.TYPE,
+//                String.class,
+//                String.class,
+//                String.class,
+//                String.class,
+//                String[].class);
+//
+//        XposedBridge.log("-- find method, method: " + start.toString());
+//
+//        XposedBridge.hookMethod(start, debugAppsHook);
+
+    }
+
+    @Override
+    public void initZygote(final IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
+        XposedBridge.hookAllMethods(Process.class, "start", debugAppsHook);
+    }
+}
